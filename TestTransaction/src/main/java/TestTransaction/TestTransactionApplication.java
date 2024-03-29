3a6cde23b0b02dc52ee8afeface77e8a38f5422d
Neskj <pronin.qq@gmail.com>
2024-03-30 00:21:29 +0300

    start proj

---
+++ TestTransaction/src/main/java/TestTransaction/TestTransactionApplication.java
@@ -0,0 +1,13 @@
+package TestTransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTransactionApplication.class, args);
+	}
+
+}
